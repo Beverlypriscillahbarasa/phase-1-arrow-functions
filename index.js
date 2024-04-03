9f768872346fff170161e4a9491efbbd1b266153
priscillah barasa <beverlybarasa@gmail.com>
2024-04-04 01:25:34 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = (a , b) => {
+    console.log( `a/b`);
+    return a/b;
+    }
+   
+divide(2000, 100)
+
+const square = (c) => {
+    console.log (`c*c`);
+    return c*c;
+
+}
+square(2)
+
+const add = (d, e) => {
+    console.log( d + e);
+    return d+e
+}
+add (3,4)
